54c42b7f2054eec2dbd65048fedff5db4c776d94
RudraRaiyani18 <raiyanirudra5@gmail.com>
2026-02-08 23:52:07 +0530

    menuItems

--- client/src/pages/admin/dashboard/Dashboard.jsx
+++ client/src/pages/admin/dashboard/Dashboard.jsx
@@ -1,7 +1,31 @@
 import { react } from "react";
 
 const AdminPage= () =>{
+    const menuItems = [
+    {
+      icon: BarChart,
+      label: "Dashboard",
+      value: "dashboard",
+      component: <InstructorDashboard listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: Book,
+      label: "Courses",
+      value: "courses",
+      component: <InstructorCourses listOfCourses={instructorCoursesList} />,
+    },
+    {
+      icon: LogOut,
+      label: "Logout",
+      value: "logout",
+      component: null,
+    },
+  ];
 
+  function handleLogout() {
+    resetCredentials();
+    sessionStorage.clear();
+  }
 }
 
 export default AdminPage;
